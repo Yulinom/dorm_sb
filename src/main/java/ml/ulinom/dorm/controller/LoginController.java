9f215e55e0d973c0ca6465f23a9f06f206919df4
ygh <chygh2000@163.com>
2022-07-26 15:27:46 +0800

    1、登陆 2、住宿修改

---
+++ src/main/java/ml/ulinom/dorm/controller/LoginController.java
@@ -0,0 +1,25 @@
+package ml.ulinom.dorm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String longin(String user, String password, HttpSession session) {
+        session.setAttribute("token", UUID.randomUUID());
+        if (user.equals("admin") && password.equals("admin")) {
+            return "index";
+        } else return "login";
+
+    }
+
+    @RequestMapping("/ini")
+    public String index() {
+        return "login";
+    }
+}
